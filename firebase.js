5ae431dda483371d277c3399bba80116f6655e61
Shivam Karn <shivam3511@gmail.com>
2023-04-19 01:09:14 +0530

    fixed firebase

--- firebase.js
+++ firebase.js
@@ -1,8 +1,19 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("./firebase-adminsdk.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    "type": "service_account",
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY,
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": process.env.AUTH_URI,
+    "token_uri": process.env.TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_CERT,
+    "client_x509_cert_url": process.env.CLIENT_CERT
+  }
+  ),
   databaseURL: process.env.FIREBASE_DB_URL
 });
 
