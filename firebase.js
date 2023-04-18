05d6ee3ea806ca05678c01e4742de164b6594504
Shivam Karn <shivam3511@gmail.com>
2023-04-19 01:24:55 +0530

    fix firebase v2.0

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ admin.initializeApp({
     "type": "service_account",
     "project_id": process.env.PROJECT_ID,
     "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY,
+    "private_key": process.env.PRIVATE_KEY.replace(/\n/gm, "\n"),
     "client_email": process.env.CLIENT_EMAIL,
     "client_id": process.env.CLIENT_ID,
     "auth_uri": process.env.AUTH_URI,
